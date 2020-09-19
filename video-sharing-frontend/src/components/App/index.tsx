626854309fc5eef9aab1cde11af277f5b9f0f807
Dejanvuk <dejan.vk@outlook.com>
2020-09-19 23:17:41 +0300

    added frontend archetype

---
+++ video-sharing-frontend/src/components/App/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+export default function App() {
+    return <Button color="primary">Video Share</Button>;
+}
---
+++ video-sharing-frontend/webpack.config.js
@@ -0,0 +1,66 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const webpack = require('webpack');
+
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.tsx",
+    devtool: 'cheap-module-source-map',
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(j|t)sx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader", 
+                options: {
+                    cacheDirectory: true,
+                    babelrc: false,
+                    presets: [
+                        [
+                            "@babel/preset-env",
+                            { targets: { browsers: "last 2 versions" } } 
+                        ],
+                        "@babel/preset-typescript",
+                        "@babel/preset-react"
+                    ],
+                    plugins: [
+                        ["@babel/plugin-proposal-decorators", { legacy: true }],
+                        ["@babel/plugin-proposal-class-properties", { loose: true }],
+                        "react-hot-loader/babel"
+                    ]
+                }
+            },
+            {
+                test: /\.css$/, use: [
+                    {loader: 'style-loader'}, 
+                    {loader: 'css-loader'} 
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif)$/, use: [{loader: 'file-loader'}] 
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: {
+            index: '/',
+        },
+        hot: true
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new ForkTsCheckerWebpackPlugin()
+    ]
+};
